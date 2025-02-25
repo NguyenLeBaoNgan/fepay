0f2f134acdcdbaa95076153ad71a38195d02a098
NguyenLeBaoNgan <127002255+NguyenLeBaoNgan@users.noreply.github.com>
2025-02-25 09:27:12 +0700

    up
    ~                                                                                                                                                                                                                        .git/COMMIT_EDITMSG [unix] (09:33 25/02/2025)                                                                                                                                                                    14,1 All
    "/d/testdacx/be-4/clonefe/autopay-fe/autopay-fe/.git/COMMIT_EDITMSG" [unix] 14L, 395B

--- utils/axiosClient.ts
+++ utils/axiosClient.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosClient = axios.create({
-   baseURL: "https://api.nganbao.id.vn",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/",
+  //  baseURL: "https://api.nganbao.id.vn",
   // baseURL: "http://127.0.0.1:8000/",
   // baseURL: "https://5d31-1-53-82-217.ngrok-free.app",
   withCredentials: true,
